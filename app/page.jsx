723e987f5d241cf4a33bd7368b43436ee917d5e7
Eunum56 <msiaffu123@gmail.com>
2024-11-23 12:53:59 +0530

    fixing fetch

--- app/page.jsx
+++ app/page.jsx
@@ -4,31 +4,40 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
   const [fetching, setFetching] = useState(true);
-
   const [Posts, setPosts] = useState([]);
 
   const fetchPost = async () => {
-    const response = await fetch("/api/post", {
-      method: "GET",
-      headers: {
-        cache: "no-store",
-      },
-    });
-    const data = await response.json();
+    try {
+      const response = await fetch("/api/post", {
+        method: "GET",
+        headers: {
+          "Cache-Control": "no-store",
+        },
+      });
 
-    setPosts(data.reverse());
-    setFetching(false);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setPosts(data.reverse());
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    } finally {
+      setFetching(false);
+    }
   };
 
   useEffect(() => {
     fetchPost();
   }, []);
+
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="green_gradient text-center"> Accross the world</span>
+        <span className="green_gradient text-center"> Across the world</span>
       </h1>
       <Feed Posts={Posts} fetching={fetching} />
     </section>
