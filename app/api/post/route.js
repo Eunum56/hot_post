8dea34642e27490f760b3ea002ece9018a19dcee
Eunum56 <msiaffu123@gmail.com>
2024-11-23 12:56:52 +0530

    fixing fetch

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -1,25 +1,13 @@
-import Post from "@models/post";
-import { connectDB } from "@utils/db";
+import Post from "@models/post"
+import { connectDB } from "@utils/db"
 
-export const GET = async (req, { params }) => {
+export const GET = async (req, res) => {
     try {
-        await connectDB();
+        await connectDB()
         const posts = await Post.find({}).populate('creator');
 
-        const headers = new Headers();
-        headers.append('Cache-Control', 'no-store');
-
-        return new Response(JSON.stringify(posts), {
-            status: 200,
-            headers: headers,
-        });
+        return new Response(JSON.stringify(posts), { status: 200, headers: { 'Cache-Control': 'no-store' } })
     } catch (error) {
-        const headers = new Headers();
-        headers.append('Cache-Control', 'no-store');
-
-        return new Response("Failed to load all posts", {
-            status: 500,
-            headers: headers,
-        });
+        return new Response("Failed to load all posts", { status: 500, headers: { 'Cache-Control': 'no-store' } })
     }
-};
+}
--- app/page.jsx
+++ app/page.jsx
@@ -4,40 +4,31 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
   const [fetching, setFetching] = useState(true);
+
   const [Posts, setPosts] = useState([]);
 
   const fetchPost = async () => {
-    try {
-      const response = await fetch("/api/post", {
-        method: "GET",
-        headers: {
-          "Cache-Control": "no-store",
-        },
-      });
+    const response = await fetch("/api/post", {
+      method: "GET",
+      headers: {
+        cache: "no-store",
+      },
+    });
+    const data = await response.json();
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const data = await response.json();
-      setPosts(data.reverse());
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-    } finally {
-      setFetching(false);
-    }
+    setPosts(data.reverse());
+    setFetching(false);
   };
 
   useEffect(() => {
     fetchPost();
   }, []);
-
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Discover & Share
         <br className="max-md:hidden" />
-        <span className="green_gradient text-center"> Across the world</span>
+        <span className="green_gradient text-center"> Accross the world</span>
       </h1>
       <Feed Posts={Posts} fetching={fetching} />
     </section>
